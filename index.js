382ad50978568c17dee84cc46f229bcc84ad5bbb
spodder <spodder37@gmail.com>
2022-05-27 18:11:34 +0600

    all parts api added

--- index.js
+++ index.js
@@ -3,9 +3,43 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://parts_user:HSwcnx6mJhINi5wX@cluster0.9ri7q.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("partsDb connected...")
+    const partsCollection = client.db("partsDB").collection("parts");
+
+    // get all the parts api 
+    app.get('/parts', async (req, res) => {
+      const query = {}
+      const cursor = partsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // get a single part 
+
+
+
+
+
+
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send(' Manufacturer website server running...');
