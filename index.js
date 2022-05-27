a28c7b1ab408bd9aeaffc099faeffdb66e0308a0
spodder <spodder37@gmail.com>
2022-05-27 19:13:43 +0600

    single item api added

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ async function run() {
     })
 
     // get a single part 
-
+    app.get('/part/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const part = await partsCollection.findOne(query);
+      res.send(part);
+    })
 
 
 
