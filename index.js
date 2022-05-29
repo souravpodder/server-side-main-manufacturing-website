a0cd54e19766d609bab033ef71fc9994b0ddccd3
spodder <spodder37@gmail.com>
2022-05-29 20:18:45 +0600

    admin security implemented

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
+  // console.log(authHeader);
   if (!authHeader) {
     return res.status(401).send({ message: 'Unauthorized Access' });
   }
